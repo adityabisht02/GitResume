36b818c6e1c883136df4effe53e7f1cb79715ab4
Aditya <adityabisht02@gmail.com>
2023-04-01 00:53:53 +0530

    added sample resume html template

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+const profile = {
+  name: "Aditya",
+  link: "https://github.com/adityabisht02",
+  profilepic: "https://avatars.githubusercontent.com/u/89146189?v=4",
+};
 app.get("/", function (req, res) {
-  res.render("index");
+  res.render("index", profile);
+});
+app.get("/resume", function (req, res) {
+  res.render("resume1", profile);
 });
 
 app.listen(3000, function () {
