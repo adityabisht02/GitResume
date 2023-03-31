4755cc565754b9352b0b5eb2cd45e29de42cc9d4
Aditya <adityabisht02@gmail.com>
2023-03-31 22:23:44 +0530

    setup tailwindcss and views directory

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
+const ejs = require("ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
 app.listen(3000, function () {
   console.log("Server listening on port 3000");
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,css,js}", "./views/*.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
